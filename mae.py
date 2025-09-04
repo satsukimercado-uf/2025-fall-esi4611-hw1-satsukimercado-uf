b9c330a0b19a1c44090346b7baeb1125e2cb4f5e
satsukimercado-uf <satsukimercado@ufl.edu>
2025-09-04 18:28:31 +0000

    Calcaulted mean and variance for x and y values

--- mae.py
+++ mae.py
@@ -4,13 +4,39 @@ import numpy as np
 
 # def main
 def main(): 
-    df = read_csv()
-    mean()
+    df_in = read_csv()
+    mean(df)
 
 # def read_csv
-def function read_csv():
-    df = pd.read_csv("quartet.csv")
+def read_csv():
+    # store file
+    df_in = pd.read_csv("quartet.csv")
     return df
+
+# def mean
+def mean(df_in):
+    # store input
+    df_in = df_in
+    # store output
+    df_mn = pd.DataFrame () # 1x8 matrix
+    df_var = pd.DataFrame() # 1x8 matrix 
+    df_out = pd.DataFrame(columns = ['x_mean', 'y_mean', 'x_var', 'y_var', 'corr'])
+
+
+    # df.shape[0] > row count, m = 0. df.shape[1] > column count, n = 1. 
+   
+    # find mean and var values
+    for i in range(df_in.shape[1]): # for i = 0 to num of columns 
+        # mean 
+        mn = df_in.column[i].mean()
+        # var 
+        var = df_in.column[i].var()
+        # insert in entry [0,i] in output df
+        df_mn.loc[0,i] = mn
+        df_var.loc[0,i] = var
+
+    print(df_mn, "\n", df_var)
+
 
 # start
 if __name__ == "__main__":
